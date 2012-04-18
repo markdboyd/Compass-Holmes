ed911bcd4738a8b55b0ef011226ad78edd9b9387
Mark Boyd <markdavidboyd@gmail.com>
2012-04-17 22:58:53 -0400

    first commit

---
+++ templates/project/manifest.rb
@@ -0,0 +1,19 @@
+description "A SASS version of the Holmes Markup detective"
+
+# Make sure you list all the project template files here in the manifest.
+stylesheet '_holmes.scss', :media => 'screen, projection'
+
+
+help %Q{
+Docs on the compass port of blueprint can be found on the wiki:
+
+    http://wiki.github.com/chriseppstein/compass/blueprint-documentation
+}
+
+welcome_message %Q{
+Docs on the compass port of blueprint can be found on the wiki:
+
+    http://wiki.github.com/chriseppstein/compass/blueprint-documentation
+
+To get started, edit the screen.sass file and read the comments and code there.
+}
